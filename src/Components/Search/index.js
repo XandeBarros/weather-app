52a7f297a4b50426d7fbda9741370ee966d03d40
Xande Barros <barrosalex6@gmail.com>
2022-02-09 17:18:05 -0300

    Solving Bugs #001 - Pesquisa com Enter Key | Implantar React direto no Body | Comando yarn dev

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container } from './styles.js';
 
 export default function Search({ place, handleFetch, handleSearch }) {
+  useEffect(() => {
+    const listener = event => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        event.preventDefault();
+        handleFetch();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   return(
     <Container>
       <input type="text" value={place} onChange={handleSearch}></input>
